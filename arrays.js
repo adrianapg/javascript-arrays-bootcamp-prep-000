e8fabeeaa2c858da3aca0d77aaf1fc634a242905
Adriana Perez <adriana.perez.garcia@outlook.com>
2018-04-03 02:08:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function addElementToEndOfArray(array, element){
   return modified_array
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
+function destructivelyAddElementToEndOffArray(array, element){
+  array.push(element)
   return array
 }
